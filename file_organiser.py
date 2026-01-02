32eabc1bcf78b6bd94adc39110d5e5201806927f
Maksymilian Nowakowski <maks.nowakowski.pro@gmail.com>
2026-01-02 14:06:17 +0000

    Feature: Create categories

--- file_organiser.py
+++ file_organiser.py
@@ -6,5 +6,17 @@ def organise_files(path):
     for file in files:
         print("-", file)
 
+    categories = {
+    "Text Files": [".txt"],
+    "Images": [".jpg", ".png", ".jpeg"],
+    "PDFs": [".pdf"]
+    }
+
+    for category in categories:
+        folder = os.path.join(path, category)
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+            print(f"Created folder: {category}")
+
 folder_path = input("Enter folder path to organise: ")
 organise_files(folder_path)
